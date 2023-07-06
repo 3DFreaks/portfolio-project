57c0727e904cce0c7896c0418791d49856ea1765
Jan <jan.romanowski2@gmail.com>
2023-07-06 12:52:35 +0200

    Added Footer and fixed StartButton centering

--- app/components/StartButton.tsx
+++ app/components/StartButton.tsx
@@ -4,8 +4,23 @@ import { IButtonProps } from "../types";
 
 export const StartButton : FC<IButtonProps> = (props): JSX.Element => {
   let clicked : boolean = false
+  let buttonClass= `absolute 
+                    top-1/2 
+                    left-1/2 
+                    bg-blue-500 
+                    hover:bg-blue-700 
+                    text-white 
+                    font-bold 
+                    py-2 
+                    px-4 
+                    rounded 
+                    w-32 
+                    h-10 
+                    -translate-y-5
+                    -translate-x-16
+                    `
   return <Button 
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className={buttonClass}
           onMouseDown={()=>{clicked = true}}
           onMouseUpCapture={()=>
             { 
